7029f2eba25e82280be42eb8501a206bd0c0fb53
01402035 <1311842831@qq.com>
2020-11-03 17:15:47 +0800

    更新

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+
 
 # Create your views here.
+
+def demo(request):
+    return render(request, 'demo.html')
+
+
+def page(request, num):
+    try:
+        num = int(num)
+        return render(request, 'demo.html')
+    except:
+        raise Http404
+
+
+def home(request, year='2020', month='11'):
+    return HttpResponse('获 取当前时间页签%s年/%s月' % (year, month))
+
+
+def home1(request, year='2020', month='11'):
+    return HttpResponse('获 取当前时间页签%s年/%s月' % (year, month))
+
+
+def hom(request):
+    return render(request, 'home.html')
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,9 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from    django.conf.urls    import url
+from django.conf.urls import url
+from demo import views
 from . import view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', view.hello)
+    #精确匹配
+    path('page/<year>/<month>', views.home),
+    url(r'^$', view.hello),
+    url('^demo/$', views.demo,name='demo_page'),
+    url(r'^page/(\d+)$', views.page),
+    # 获取urlc参数,用<name>这种格式,?P 参数year
+    # [0-9] 匹配0-9的数字
+    # {4} 匹配4个数字
+    # {1,2} 匹配1-2个数字
+    # r 是raw原型，不转义
+    # ^ 匹配开始
+    # $ 匹配结束
+    url(r'^page/(?P<year>[0-9]{4})/(?P<month>[0-9]{1，2}).html$', views.home1),
+    url('hom/',views.hom,name='home_page')
 ]
